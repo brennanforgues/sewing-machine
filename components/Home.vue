26a58dc7c6f41ad3297c0f265439a4b51c84ebd6
Forgues,Brennan <brennan.forgues@gmail.com>
2017-08-23 22:58:50 -0400

    added buttons for spotify, google placy and apple music

--- components/Home.vue
+++ components/Home.vue
@@ -1,5 +1,5 @@
 <template>
-  <main class="w-100 cf helvetica dark-gray bg-white pa3 pa4-m pa5-l mw9 center brennan">
+  <main class="w-100 cf helvetica dark-gray bg-white pa3 pa4-m pa5-l mw9 center">
     <div class="fl w-50 pr2 pr3-l mb3 mb4-l">
       <div class="cover pv5 pv6-m pv7-l" style="background:black url(https://static.pexels.com/photos/48592/pexels-photo-48592.jpeg) center;"></div>
     </div>
@@ -13,8 +13,8 @@
     <div class="fl w-100 mobile-w-100 pl2 pl0-l pr2-m mb3 mb4-l">
       <div class="cover pv5 pv6-m pv7-l" style="background:black url(https://static1.squarespace.com/static/557903b5e4b0daf0f9cbfe1b/584e08b9d482e9382076d992/584e08e7d2b8572e194d1020/1481509122225/untitled-428+copy+3.jpg)"> </div>
     </div>
-    <div class="fl w-100 w-100-l mb4 mb0-l outline">
-      <div class="fl w-50 w-50-1 mt3 mb1 v-mid">
+    <div class="fl w-100 w-100-l mobile-w-100 mb3 mb4-l outline">
+      <div class="fl w-50 w-50-1 mt3 mb1">
         <h1 class="f4 f2-l fw7 mt0 pv3-l bb-l bb--black">No.1</h1>
         <p class="lh-copy mt2 mt3-m mt5-l f6">
           <span class="db-ns f6 fw7 lh-solid mb3 mb0-m mb4-l">The Debut EP</span>
@@ -25,6 +25,23 @@
       </div>
       <div class="fl w-50 w-50-1 mt3 mb1 mb4-l">
         <div class="cover pv5 pv6-m pv7-l" style="background:black url(http://is1.mzstatic.com/image/thumb/Music71/v4/10/7a/42/107a424a-23da-1cb6-be10-0cb319cd20ca/source/1200x630bb.jpg) center;"></div>
+      </div>
+      <div class="fl w-100 w-100-l cf">
+          <div class="fl w-100 w-33-m w-33-l tc pv4">
+            <a href="https://open.spotify.com/artist/7KYkxnEJDG3eFn3eoOjsD8" class="f5 no-underline black bg-animate hover-bg-black hover-white inline-flex items-center pa3 ba border-box mr4">
+              <span class="pl1">SPOTIFY</span>
+            </a>
+          </div>
+          <div class="fl w-100 w-33-m w-33-l tc pv4">
+            <a href="https://play.google.com/store/music/album/Kaitlyn_Zarzour_No_1?id=Bhdgdawehik2tp62dunvmz4eljy" class="f5 no-underline black bg-animate hover-bg-black hover-white inline-flex items-center pa3 ba border-box mr4">
+              <span class="pl1">GOOGLE PLAY</span>
+            </a>
+          </div>
+          <div class="fl w-100 w-33-m w-33-l tc pv4">
+            <a href="https://itunes.apple.com/us/album/no-1-ep/id1180690396?ign-mpt=uo%3D5" class="f5 no-underline black bg-animate hover-bg-black hover-white inline-flex items-center pa3 ba border-box mr4">
+              <span class="pl1">ITUNES</span>
+            </a>
+          </div>
       </div>
     </div>
   </main>
