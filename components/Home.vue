3a3045f88429aaba2b90f7a1b08f8a7d9cd64bfc
Forgues,Brennan <brennan.forgues@gmail.com>
2017-08-23 21:54:59 -0400

    redesigned some of the css grid layouts

--- components/Home.vue
+++ components/Home.vue
@@ -1,29 +1,30 @@
 <template>
-  <main class="w-100 cf helvetica dark-gray bg-white pa3 pa4-m pa5-l mw9 center">
+  <main class="w-100 cf helvetica dark-gray bg-white pa3 pa4-m pa5-l mw9 center brennan">
     <div class="fl w-50 pr2 pr3-l mb3 mb4-l">
       <div class="cover pv5 pv6-m pv7-l" style="background:black url(https://static.pexels.com/photos/48592/pexels-photo-48592.jpeg) center;"></div>
     </div>
     </div>
-    <div class="fl w-50 w-25-l pl2 pr2-m ph2-l mb3 mb4-l">
-      <div class="cover pv5 pv6-m pv7-l" style="background:black url(https://static.pexels.com/photos/185030/pexels-photo-185030.jpeg) center;"> </div>
+    <div class="fl w-25 w-25-l mobile-w-50 pl2 pr2-m ph2-l mb3 mb4-l">
+      <div class="cover pv5 pv6-m pv7-l" style="background:black url(https://static1.squarespace.com/static/557903b5e4b0daf0f9cbfe1b/584e08b9d482e9382076d992/584e08d0f5e2310b39755f32/1481509102092/untitled-375+copy+3.jpg) top;"> </div>
     </div>
-    <div class="fl w-50 w-50 w-25-l pr2 pr0-l pl3-l mb3 mb4-l">
-        <div class="cover pv5 pv6-m pv7-l" style="background:black url(http://mrmrs.github.io/photos/050.jpg) left;"> </div>
+    <div class="fl w-25 w-25-l mobile-w-100 pr2 pr0-l pl3-l mb3 mb4-l">
+        <div class="cover pv5 pv6-m pv7-l" style="background:black url(https://static.pexels.com/photos/185030/pexels-photo-185030.jpeg) center;"> </div>
       </div>
-    <div class="fl w-50 w-50 w-25-l pl2 pl0-l pr2-m pr4-l mb3 mb4-l">
-      <div class="cover pv5 pv6-m pv7-l" style="background:black url(http://mrmrs.github.io/photos/049.jpg) center;"> </div>
+    <div class="fl w-100 mobile-w-100 pl2 pl0-l pr2-m mb3 mb4-l">
+      <div class="cover pv5 pv6-m pv7-l" style="background:black url(https://static1.squarespace.com/static/557903b5e4b0daf0f9cbfe1b/584e08b9d482e9382076d992/584e08e7d2b8572e194d1020/1481509122225/untitled-428+copy+3.jpg)"> </div>
     </div>
-    <div class="fl w-100 w-50-l pr2-l pl2-ns mb4 mb0-l mb4 outline">
-      <div class="pa4">
-        <h1 class="f4 f2-l fw7 mt0 pv3-l bb-l bb--black">#004</h1>
+    <div class="fl w-100 w-100-l pr2-l pl2-ns mb4 mb0-l mb4 outline">
+      <div class="fl w-50 pr2 pr3-l mb3 mb4-l">
+        <h1 class="f4 f2-l fw7 mt0 pv3-l bb-l bb--black">No.1</h1>
         <p class="lh-copy mt2 mt3-m mt5-l f6">
-          <span class="db-ns f6 fw7 lh-solid mb3 mb0-m mb4-l">Mies, my great mentor said:</span>
-          <span class="fw9 f6 f1-l db lh-title mb3 mb4-l">“God is in the details.”</span>
-          <span class="db-l measure-wide">
-          That is the essence of syntax: the discipline
-          that controls the proper use of grammar in the construction of phrases and the articulation of a language, Design. The syntax of design is provided by many components in the nature of the project.
-        </span>
+          <span class="db-ns f6 fw7 lh-solid mb3 mb0-m mb4-l">The Debut EP</span>
+          <span class="fw9 f6 f1-l db lh-title mb3 mb4-l">By Kaitlyn Zarzour</span>
+          <span class="db-l measure-wide"></span>
+          Listen to the debut EP by contemprary country artist Kaitlyn Zarzour from Toronto
         </p>
+      </div>
+      <div class="fl w-50 w-50-1 pr2 pr3-l mb3 mb4-l">
+        <div class="cover w-50 h3 pv5 pv6-m pv7-l" style="background:black url(https://is1-ssl.mzstatic.com/image/thumb/Music71/v4/10/7a/42/107a424a-23da-1cb6-be10-0cb319cd20ca/source/200x0w.jpg) center;"></div>
       </div>
     </div>
   </main>
@@ -32,7 +33,18 @@
 </script>
 
 <style>
- a {
-    outline: 0;
+@media screen and (max-width: 500px) {
+  .mobile-w-100 {
+      width: 100%;
+      padding-right: 0rem;
+      padding-left: 0rem;
+  }
+  .mobile-w-50 {
+    width: 50%;
+  }
+  .mobile-photo-piano {
+    top: 3rem;
+  }
 }
+
 </style>
