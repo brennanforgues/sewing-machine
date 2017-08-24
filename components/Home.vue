57fd2d45a4801d603c5db256ec8f72397cbe40a3
Forgues,Brennan <brennan.forgues@gmail.com>
2017-08-23 22:16:17 -0400

    got the album image to center properly

--- components/Home.vue
+++ components/Home.vue
@@ -14,7 +14,7 @@
       <div class="cover pv5 pv6-m pv7-l" style="background:black url(https://static1.squarespace.com/static/557903b5e4b0daf0f9cbfe1b/584e08b9d482e9382076d992/584e08e7d2b8572e194d1020/1481509122225/untitled-428+copy+3.jpg)"> </div>
     </div>
     <div class="fl w-100 w-100-l pr2-l pl2-ns mb4 mb0-l mb4 outline">
-      <div class="fl w-50 pr2 pr3-l mb3 mb4-l">
+      <div class="fl w-50 w-50-1 pr2 pr3-l mb3 mb4-l">
         <h1 class="f4 f2-l fw7 mt0 pv3-l bb-l bb--black">No.1</h1>
         <p class="lh-copy mt2 mt3-m mt5-l f6">
           <span class="db-ns f6 fw7 lh-solid mb3 mb0-m mb4-l">The Debut EP</span>
@@ -23,8 +23,8 @@
           Listen to the debut EP by contemprary country artist Kaitlyn Zarzour from Toronto
         </p>
       </div>
-      <div class="fl w-50 w-50-1 pr2 pr3-l mb3 mb4-l">
-        <div class="cover w-50 h3 pv5 pv6-m pv7-l" style="background:black url(https://is1-ssl.mzstatic.com/image/thumb/Music71/v4/10/7a/42/107a424a-23da-1cb6-be10-0cb319cd20ca/source/200x0w.jpg) center;"></div>
+      <div class="fl w-50 w-50-1 pr2 pr3-l mb3 mb4-l outline">
+        <div class="cover pv5 pv6-m pv7-l" style="background:black url(http://is1.mzstatic.com/image/thumb/Music71/v4/10/7a/42/107a424a-23da-1cb6-be10-0cb319cd20ca/source/1200x630bb.jpg) center;"></div>
       </div>
     </div>
   </main>
